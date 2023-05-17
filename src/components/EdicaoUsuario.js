e978c2dbf9a1fa031fb374f3f717b33bbdf44b78
Pedro Meerholz <developer.pedromeerholz@gmail.com>
2023-05-16 21:58:10 -0300

    Ajustando a alteração de senha

--- src/components/EdicaoUsuario.js
+++ src/components/EdicaoUsuario.js
@@ -11,13 +11,14 @@ export default (props) => {
         const credenciaisUsuario = await findUsuario(props.route.params['email']);
         console.warn(credenciaisUsuario);
         const usuario = {
-            id: credenciaisUsuario.id,
+            id: credenciaisUsuario['_array'][0]['id'],
             nome: "Usuário padrão",
-            email: credenciaisUsuario.email,
+            email: credenciaisUsuario['_array'][0]['email'],
             senha: novaSenha
         }
         // await alterPassword(props.route.params['email'], novaSenha);
-        await atualizarSenhaUsuario(usuario);
+        console.log("Usuário editado: " + JSON.stringify(usuario));
+        await atualizarSenhaUsuario(usuario, props.route.params['tokenSessao']);
     }
 
     return (
--- src/requests/request_usuario.js
+++ src/requests/request_usuario.js
@@ -1,18 +1,20 @@
 import api_key from "./api_key";
 
-export async function atualizarSenhaUsuario(usuario) {
+export async function atualizarSenhaUsuario(usuario, token) {
+    console.log("usuario: " + JSON.stringify(usuario));
     try {
-        console.log("Atualização senha");
         const response = await fetch(
             "http://ec2-54-166-238-5.compute-1.amazonaws.com/usuarios/", {
                 method: "PUT",
                 headers: {
                     'Content-type': 'application/json',
-                    'apikey': api_key
+                    'apikey': api_key,
+                    'token': token
                 },
                 body: JSON.stringify(usuario)
             }
         );
+        console.log(response);
         console.warn("Senha atualizada com sucesso");
     } catch(excecao) {
         console.log(excecao);
